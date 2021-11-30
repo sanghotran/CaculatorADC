ec791a1def6c9ba5c8d3db3279925925ed63e843
Your Name <you@example.com>
2021-11-30 19:24:18 +0700

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace CaculatorADC
+{
+    partial class frm_ADC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_ADC));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_OK = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Q = new System.Windows.Forms.TextBox();
+            this.txt_B = new System.Windows.Forms.TextBox();
+            this.txt_R = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_Value = new System.Windows.Forms.Button();
+            this.btn_Code = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_Code = new System.Windows.Forms.TextBox();
+            this.txt_Value = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbo_Approx = new System.Windows.Forms.ComboBox();
+            this.cbo_Type = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_OK);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_Q);
+            this.groupBox1.Controls.Add(this.txt_B);
+            this.groupBox1.Controls.Add(this.txt_R);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(202, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Value";
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.Location = new System.Drawing.Point(146, 22);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.Size = new System.Drawing.Size(50, 67);
+            this.btn_OK.TabIndex = 5;
+            this.btn_OK.Text = "OK";
+            this.btn_OK.UseVisualStyleBackColor = true;
+            this.btn_OK.Click += new System.EventHandler(this.btn_OK_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Q";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "B";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "R";
+            // 
+            // txt_Q
+            // 
+            this.txt_Q.Location = new System.Drawing.Point(40, 73);
+            this.txt_Q.Name = "txt_Q";
+            this.txt_Q.Size = new System.Drawing.Size(100, 20);
+            this.txt_Q.TabIndex = 2;
+            // 
+            // txt_B
+            // 
+            this.txt_B.Location = new System.Drawing.Point(40, 46);
+            this.txt_B.Name = "txt_B";
+            this.txt_B.Size = new System.Drawing.Size(100, 20);
+            this.txt_B.TabIndex = 1;
+            // 
+            // txt_R
+            // 
+            this.txt_R.Location = new System.Drawing.Point(40, 19);
+            this.txt_R.Name = "txt_R";
+            this.txt_R.Size = new System.Drawing.Size(100, 20);
+            this.txt_R.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_Value);
+            this.groupBox2.Controls.Add(this.btn_Code);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txt_Code);
+            this.groupBox2.Controls.Add(this.txt_Value);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.cbo_Approx);
+            this.groupBox2.Controls.Add(this.cbo_Type);
+            this.groupBox2.Location = new System.Drawing.Point(14, 119);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(257, 122);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ADC";
+            // 
+            // btn_Value
+            // 
+            this.btn_Value.Location = new System.Drawing.Point(166, 60);
+            this.btn_Value.Name = "btn_Value";
+            this.btn_Value.Size = new System.Drawing.Size(75, 20);
+            this.btn_Value.TabIndex = 9;
+            this.btn_Value.Text = "Value";
+            this.btn_Value.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Value.UseVisualStyleBackColor = true;
+            this.btn_Value.Click += new System.EventHandler(this.btn_Value_Click);
+            // 
+            // btn_Code
+            // 
+            this.btn_Code.Location = new System.Drawing.Point(166, 87);
+            this.btn_Code.Name = "btn_Code";
+            this.btn_Code.Size = new System.Drawing.Size(75, 20);
+            this.btn_Code.TabIndex = 8;
+            this.btn_Code.Text = "Code";
+            this.btn_Code.UseVisualStyleBackColor = true;
+            this.btn_Code.Click += new System.EventHandler(this.btn_Code_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Code";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Value";
+            // 
+            // txt_Code
+            // 
+            this.txt_Code.Location = new System.Drawing.Point(40, 87);
+            this.txt_Code.Name = "txt_Code";
+            this.txt_Code.Size = new System.Drawing.Size(120, 20);
+            this.txt_Code.TabIndex = 5;
+            // 
+            // txt_Value
+            // 
+            this.txt_Value.Location = new System.Drawing.Point(40, 60);
+            this.txt_Value.Name = "txt_Value";
+            this.txt_Value.Size = new System.Drawing.Size(120, 20);
+            this.txt_Value.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(130, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Approx";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Type";
+            // 
+            // cbo_Approx
+            // 
+            this.cbo_Approx.FormattingEnabled = true;
+            this.cbo_Approx.Location = new System.Drawing.Point(130, 33);
+            this.cbo_Approx.Name = "cbo_Approx";
+            this.cbo_Approx.Size = new System.Drawing.Size(121, 21);
+            this.cbo_Approx.TabIndex = 1;
+            // 
+            // cbo_Type
+            // 
+            this.cbo_Type.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbo_Type.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbo_Type.FormattingEnabled = true;
+            this.cbo_Type.Location = new System.Drawing.Point(6, 33);
+            this.cbo_Type.Name = "cbo_Type";
+            this.cbo_Type.Size = new System.Drawing.Size(121, 21);
+            this.cbo_Type.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CaculatorADC.Properties.Resources.icon;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(221, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 56);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frm_ADC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 252);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frm_ADC";
+            this.Text = "ADC";
+            this.Load += new System.EventHandler(this.frm_ADC_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_R;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Q;
+        private System.Windows.Forms.TextBox txt_B;
+        private System.Windows.Forms.Button btn_OK;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_Code;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_Code;
+        private System.Windows.Forms.TextBox txt_Value;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbo_Approx;
+        private System.Windows.Forms.ComboBox cbo_Type;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_Value;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaculatorADC
+{
+    public partial class frm_ADC : Form
+    {
+        public frm_ADC()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_OK_Click(object sender, EventArgs e)
+        {
+            double result;
+            result = Convert.ToDouble(txt_R.Text)/Math.Pow(2,Convert.ToDouble(txt_B.Text));
+            txt_Q.Text = result.ToString();
+        }
+
+        private void frm_ADC_Load(object sender, EventArgs e)
+        {
+            cbo_Type.Items.Add("Natural");
+            cbo_Type.Items.Add("Offset");
+            cbo_Type.Items.Add("Two’s complement");
+
+            cbo_Approx.Items.Add("Rounding");
+            cbo_Approx.Items.Add("Truncation");
+
+        }
+        private void btn_Code_Click(object sender, EventArgs e)
+        {                      
+            txt_Code.Text = CaculatorCode();
+        }
+        private void btn_Value_Click(object sender, EventArgs e)
+        {
+            txt_Value.Text = CaculatorValue();
+        }
+
+        private string CaculatorCode()
+        {
+            string code = "";
+            int mode = CheckTypeAndApprox();
+            int[] b = new int[12];
+            switch ( mode)
+            {
+                case 0:
+                    MessageBox.Show("please select Type and Approx");
+                    break;
+                case 1:
+                    for ( int i = 1; i <= Convert.ToInt32(txt_B.Text); i++ )
+                    {
+                        double value = 0;
+                        b[i-1] = 1;
+                        for (int j = 1; j <= Convert.ToInt32(txt_B.Text); j++)
+                        {
+                            value += b[j-1] * Math.Pow(2, -j);
+                        }
+                        value = value * Convert.ToDouble(txt_R.Text);
+                        if (value > Convert.ToDouble(txt_Value.Text))
+                            b[i-1] = 0;
+                        code += b[i-1].ToString();
+                    }
+                    break;
+                case 2:
+                    for (int i = 1; i <= Convert.ToInt32(txt_B.Text); i++)
+                    {
+                        double value = 0;
+                        b[i - 1] = 1;
+                        for (int j = 1; j <= Convert.ToInt32(txt_B.Text); j++)
+                        {
+                            value += b[j - 1] * Math.Pow(2, -j);
+                        }
+                        value = value * Convert.ToDouble(txt_R.Text) - 0.5 * Convert.ToDouble(txt_R.Text);
+                        if (value > Convert.ToDouble(txt_Value.Text))
+                            b[i - 1] = 0;
+                        code += b[i - 1].ToString();
+                    }
+                    break;
+                case 3:
+                    if (Convert.ToDouble(txt_Value.Text) < 0)
+                    {
+                        b[0] = 0;
+                        code += 1;
+                    }
+                    else
+                    {
+                        b[0] = 1;
+                        code += 0;
+                    }
+                    for (int i = 2; i <= Convert.ToInt32(txt_B.Text); i++)
+                    {
+                        double value = 0;
+                        b[i - 1] = 1;
+                        for (int j = 1; j <= Convert.ToInt32(txt_B.Text); j++)
+                        {
+                            value += b[j - 1] * Math.Pow(2, -j);
+                        }
+                        value = value * Convert.ToDouble(txt_R.Text) - 0.5 * Convert.ToDouble(txt_R.Text);
+                        if (value > Convert.ToDouble(txt_Value.Text))
+                            b[i - 1] = 0;
+                        code += b[i - 1].ToString();
+                    }
+                    break;
+                case 4:
+                    for (int i = 1; i <= Convert.ToInt32(txt_B.Text); i++)
+                    {
+                        double value = 0;
+                        b[i - 1] = 1;
+                        for (int j = 1; j <= Convert.ToInt32(txt_B.Text); j++)
+                        {
+                            value += b[j - 1] * Math.Pow(2, -j);
+                        }
+                        value = value * Convert.ToDouble(txt_R.Text);
+                        if (value > (Convert.ToDouble(txt_Value.Text) + Convert.ToDouble(txt_Q.Text) / 2))
+                            b[i - 1] = 0;
+                        code += b[i - 1].ToString();
+                    }
+                    break;
+                case 5:
+                    for (int i = 1; i <= Convert.ToInt32(txt_B.Text); i++)
+                    {
+                        double value = 0;
+                        b[i - 1] = 1;
+                        for (int j = 1; j <= Convert.ToInt32(txt_B.Text); j++)
+                        {
+                            value += b[j - 1] * Math.Pow(2, -j);
+                        }
+                        value = value * Convert.ToDouble(txt_R.Text) - 0.5 * Convert.ToDouble(txt_R.Text);
+                        if (value > (Convert.ToDouble(txt_Value.Text) + Convert.ToDouble(txt_Q.Text) / 2))
+                            b[i - 1] = 0;
+                        code += b[i - 1].ToString();
+                    }
+                    break;
+                case 6:
+                    if (Convert.ToDouble(txt_Value.Text) < 0)
+                    {
+                        b[0] = 0;
+                        code += 1;
+                    }
+                    else
+                    {
+                        b[0] = 1;
+                        code += 0;
+                    }
+                    for (int i = 2; i <= Convert.ToInt32(txt_B.Text); i++)
+                    {
+                        double value = 0;
+                        b[i - 1] = 1;
+                        for (int j = 1; j <= Convert.ToInt32(txt_B.Text); j++)
+                        {
+                            value += b[j - 1] * Math.Pow(2, -j);
+                        }
+                        value = value * Convert.ToDouble(txt_R.Text) - 0.5 * Convert.ToDouble(txt_R.Text);
+                        if (value > (Convert.ToDouble(txt_Value.Text) + Convert.ToDouble(txt_Q.Text) / 2))
+                            b[i - 1] = 0;
+                        code += b[i - 1].ToString();
+                    }
+                    break;
+            }
+            return code;
+        }
+        private string CaculatorValue()
+        {
+            double value = 0;
+            int mode = CheckType();
+            switch( mode)
+            {
+                case 0:
+                    MessageBox.Show("pleas select Type");
+                    break;
+                case 1:
+                    for( int i = 1; i <= txt_Code.TextLength; i++ )
+                    {
+                        value += Convert.ToDouble(txt_Code.Text[i - 1].ToString())* Math.Pow(2, -i);
+                    }
+                    value = value * Convert.ToDouble(txt_R.Text);
+                    break;
+                case 2:
+                    for (int i = 1; i <= txt_Code.TextLength; i++)
+                    {
+                        value += Convert.ToDouble(txt_Code.Text[i - 1].ToString()) * Math.Pow(2, -i);
+                    }
+                    value = value * Convert.ToDouble(txt_R.Text) - 0.5 * Convert.ToDouble(txt_R.Text);
+                    break;
+                case 3:
+                    for (int i = 1; i <= txt_Code.TextLength; i++)
+                    {
+                        if( i == 1)
+                        {
+                            if (Convert.ToInt16(txt_Code.Text[0].ToString()) == 0)
+                                value += 1 * Math.Pow(2, -1);
+                            else value += 0;
+                        }
+                        else
+                        value += Convert.ToDouble(txt_Code.Text[i - 1].ToString()) * Math.Pow(2, -i);
+                    }
+                    value = value * Convert.ToDouble(txt_R.Text) - 0.5 * Convert.ToDouble(txt_R.Text);
+                    break;
+            }
+
+            return value.ToString();
+        }
+
+        private int CheckTypeAndApprox()
+        {
+            int mode = 0;
+            mode = CheckType();
+            if(cbo_Approx.SelectedItem.ToString() == "Truncation")
+            {
+                switch( mode)
+                {
+                    case 1:
+                        return 1;
+                    case 2:
+                        return 2;
+                    case 3:
+                        return 3;
+                }
+            }
+            else if (cbo_Approx.SelectedItem.ToString() == "Rounding")
+            {
+                switch (mode)
+                {
+                    case 1:
+                        return 4;
+                    case 2:
+                        return 5;
+                    case 3:
+                        return 6;
+                }
+            }
+            return 0;
+        }
+
+        private int CheckType()
+        {
+            if (cbo_Type.SelectedItem.ToString() == "Natural")
+            {
+                return 1;
+            }
+            else if (cbo_Type.SelectedItem.ToString() == "Offset")
+            {
+                return 2;
+            }
+            else if (cbo_Type.SelectedItem.ToString() == "Two’s complement")
+            {
+                return 3;
+            }
+            return 0;
+        }
+
+        
+    }
+}
